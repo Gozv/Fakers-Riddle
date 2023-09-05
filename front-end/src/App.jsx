6b2587841e881b44b30a4ac3df47780ab7b622de
martina <martinavargass04@gmail.com>
2023-09-05 20:17:51 -0300

    initial commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.modules.css'
+
+function Footer () {
+  return(
+    <div>
+        <h1>hola mundo</h1>
+    </div>
+  )
+}
+
+export default Footer
