ab9527b0a24307e98e54e7c9671c4a59321fe1c4
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-05 23:13:48 -0300

    Add component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from './components/Footer'
 import './App.css'
+import GameHistory from './components/GameHistory'
 
 function App() {
   return (
     <>
+      <GameHistory />
       <Footer />
     </>
   )
