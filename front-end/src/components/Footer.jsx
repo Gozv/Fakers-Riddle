46899c40bbf34d2a119fd62ce35d5edc281b2161
martina <martinavargass04@gmail.com>
2023-09-07 17:02:16 -0300

    add footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,11 +1,63 @@
 import React from "react";
+import githubLogo from '../assets/img/logo-github.svg'
+import linkedinLogo from '../assets/img/linkedin.svg'
 import './Footer.modules.css'
 
 function Footer () {
   return(
-    <div>
-        <h1>hola mundo</h1>
-    </div>
+    <>
+        <h1>About Us</h1>
+
+        <section className="members">
+          <div className="member">
+            <p>Gonzalo Vignolles</p>
+            <div className="images">
+              <a href="https://github.com/Gozv">
+                <img src={githubLogo} alt="githubLogo"/>
+              </a>
+              <a href="">
+                <img src={linkedinLogo} alt="githubLogo"/>
+              </a>
+            </div>
+          </div>
+
+          <div className="member">
+            <p>Anahi Flores</p>
+            <div className="images">
+              <a href="">
+                <img src={githubLogo} alt="githubLogo"/>
+              </a>
+              <a href="">
+                <img src={linkedinLogo} alt="githubLogo"/>
+              </a>
+            </div>
+          </div>
+
+          <div className="member">
+            <p>Antonela Ledesma</p>
+            <div className="images">
+              <a href="https://github.com/AntonelaLedesma7">
+                <img src={githubLogo} alt="githubLogo"/>
+              </a>
+              <a href="">
+                <img src={linkedinLogo} alt="githubLogo"/>
+              </a>
+            </div>
+          </div>
+
+          <div className="member">
+            <p>Martina Vargas</p>
+            <div className="images">
+              <a href="https://github.com/martinavargas">
+                <img src={githubLogo} alt="githubLogo"/>
+              </a>
+              <a href="">
+                <img src={linkedinLogo} alt="githubLogo"/>
+              </a>
+            </div>
+          </div>
+        </section>
+    </>
   )
 }
 
