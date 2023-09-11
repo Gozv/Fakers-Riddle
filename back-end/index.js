5455d5ae403ee662ff31513b1f7c8c7d186ffaa9
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-11 18:04:33 -0300

    Add routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,16 +1,31 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import { expressjwt as jwt } from 'express-jwt'
+import errorHandler from './middlewares/errorHandler.js'
+import { authRoutes } from './router/authRouter.js'
+import { userRoutes } from './router/userRouter.js'
+import { characterRoutes } from './router/characterRouter.js'
+import { gameRoutes } from './router/gameRouter.js'
+import { gameUserRoutes } from './router/gameUserRouter.js'
+import { roomRoutes } from './router/roomRouter.js'
+import { messageRoutes } from './router/messageRouter.js'
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(
+  jwt({
+    secret: process.env.SECRET_KEY,
+    algorithms: ['HS256']
+  }).unless({ path: ['/api'] })
+)
+
 app.use(express.json())
 
-app.use('/api', (requet, response) => {
-  return response.json({ message: 'HOLAA EN EL SERVIDOR' })
-})
+app.use('/api', authRoutes(), userRoutes(), characterRoutes(), roomRoutes(), gameRoutes(), gameUserRoutes(), messageRoutes())
+
+app.use(errorHandler)
 
 app.listen(PORT)
