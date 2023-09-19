6218dd5c52cc7bd3c64aafc2c61b1097b0a42394
martina <martinavargass04@gmail.com>
2023-09-19 18:08:33 -0300

    add new set for available rooms

--- back-end/socket.js
+++ back-end/socket.js
@@ -1,8 +1,14 @@
 const socketServer = (io) => {
+  const availableRooms = new Set()
+
   io.on('connection', (socket) => {
     socket.on('joinRoom', (roomName) => {
       socket.join(roomName)
       console.log(`Usuario ${socket.id} se unió a la sala: ${roomName}`)
+
+      availableRooms.add(roomName)
+
+      io.emit('availableRooms', Array.from(availableRooms))
 
       socket.on('message', (body) => {
         console.log(`Mensaje recibido: ${body} de ${socket.id}`)
@@ -11,14 +17,16 @@ const socketServer = (io) => {
           from: socket.id.slice(6)
         })
       })
-
-      socket.on('connect', () => {
-        console.log(`Conectado a la sala ${roomName}`)
-      })
-
       socket.on('disconnect', () => {
         console.log(`Desconectado de la sala ${roomName}`)
       })
+
+      const roomEmpty = !io.sockets.adapter.rooms.get(roomName)
+      if (roomEmpty) {
+        availableRooms.delete(roomName)
+
+        io.emit('availableRooms', Array.from(availableRooms))
+      }
     })
   })
 }
--- front-end/src/components/Rooms.jsx
+++ front-end/src/components/Rooms.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
+
+const socket = io('http://localhost:3000')
 
 function Rooms () {
     const [roomName, setRoomName] = useState('')
-
+    const [availableRooms, setAvailableRooms] = useState([])
     const handleRoomNameChange = (e) => {
       setRoomName(e.target.value)
     }
+    
 
     const handleEnterPress = (e) => {
-        if (e.key === 'Enter' && roomName.trim() !== '') {
-            window.location = `room/${roomName}`
-          }
+      if (e.key === 'Enter' && roomName.trim() !== '') {
+          window.location = `room/${roomName}`
+        }
     }
+
+    const goToRoom = () => {
+      if (roomName.trim() != "") {
+        window.location = `room/${roomName}`
+      }
+    }
+
+    useEffect(() => {
+        socket.on('availableRooms', (rooms) => {
+          setAvailableRooms(rooms)
+        })
+    
+        return () => {
+          socket.off('availableRooms')
+        }
+      }, [])
 
     return(
         <div className='rooms-container'>
             <input type="text" placeholder='Write a name for the room' value={roomName} onChange={handleRoomNameChange} onKeyPress={handleEnterPress} className='border-2 border-zinc-500 p-2 w-full text-black'/>
-            <Link to={`room/${roomName}`}>Join room</Link>
+            <button onClick={goToRoom}>Join room</button>
+            <br />
+            <div>
+                <p>Salas Disponibles:</p>
+                <ul>
+                {availableRooms.map((room) => (
+                    <li key={room}>
+                    <a href={`room/${room}`}>{room}</a>
+                    </li>
+                ))}
+                </ul>
+            </div>
         </div>
     )
 }
