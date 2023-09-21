686b7a7c6dc63ec26731a3789b171ee35af0de02
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-20 21:20:45 -0300

    Add fetch data

--- front-end/src/routes/login.jsx
+++ front-end/src/routes/login.jsx
@@ -1,6 +1,25 @@
 import { Form, redirect } from "react-router-dom"
 
-export async function action () {
+export async function action ({ request }) {
+  const formData = await request.formData();
+  const loginData = Object.fromEntries(formData);
+  const response = await fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  });
+  const data = await response.json()
+  
+  if (data.token) {
+    window.localStorage.setItem('token', data.token);
+  }
+
+  if(data.refreshToken) {
+    window.localStorage.setItem('refreshToken', data.refreshToken);
+  }
+  
   return redirect('/room')
 }
 
@@ -14,21 +33,25 @@ function Login() {
       >
         <h2 className="text-3xl font-semibold text-center mb-4">Login</h2>
         <label className="block text-xl py-6">
-          Email:
-          <input
-            type="email"
-            placeholder="JoeDoe@email.com"
-            required
-          />
-        </label>
-        <label className="block text-xl py-6">
-          Password:
-          <input
-            type="password"
-            placeholder=" ◽◽◽◽◽◽◽◽"
-            required
-          />
-        </label>
+            Email:
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="JoeDoe@email.com"
+              required
+            />
+          </label>
+          <label className="block text-xl py-6">
+            Password:
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder=" ◽◽◽◽◽◽◽◽"
+              required
+            />
+          </label>
         <button
             className="w-full item-center bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded"
             type="submit"
