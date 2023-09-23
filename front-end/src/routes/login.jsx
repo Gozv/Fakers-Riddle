7928ef36fb84fda61685e06ae98c7fd6f5400b48
Gonzalo <gonzalosantacruzariel@gmail.com>
2023-09-23 13:28:20 -0300

    cambio el login a Mui

--- front-end/src/routes/login.jsx
+++ front-end/src/routes/login.jsx
@@ -1,4 +1,6 @@
-import { Form, redirect } from "react-router-dom"
+import { ThemeProvider, createTheme, TextField, Grid, Button } from '@mui/material';
+import { redirect } from "react-router-dom"
+import { useState } from 'react';
 
 export async function action ({ request }) {
   const formData = await request.formData();
@@ -23,44 +25,133 @@ export async function action ({ request }) {
   return redirect('/room')
 }
 
+const theme = createTheme();
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({
+    emailError: false,
+    passwordError: false,
+    emailMessage: "",
+    passwordMessage: "",
+  });
+
+  const validateEmail = (email) => {
+    const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    return regex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+    return regex.test(password);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let emailValid = validateEmail(email);
+    let passwordValid = validatePassword(password);
+
+    setError({
+      emailError: !emailValid,
+      passwordError: !passwordValid,
+      emailMessage: emailValid ? "" : "Email incorrecto",
+      passwordMessage: passwordValid ? "" : "Contraseña incorrecta",
+    });
+
+    if (emailValid && passwordValid) {
+      console.log("Inicio de sesión exitoso");
+      // Aquí puedes realizar la autenticación o enviar los datos al servidor
+    }
+  };
+
   return (
-    <section>
-      <Form 
-        method="post"
-        className="px-20 py-8 font-serif text-base/10 border rounded-5xl shadow-md content-between"
-      >
-        <h2 className="text-3xl font-semibold text-center mb-4">Login</h2>
-        <label className="block text-xl py-6">
-            Email:
-            <input
+    <ThemeProvider theme={theme}>
+      <section>
+        <form
+          method="post"
+          className="px-20 py-8 font-serif text-base/10 border rounded-5xl shadow-md content-between"
+          onSubmit={handleSubmit}
+        >
+          <h2 className="text-3xl font-semibold text-center mb-4">Login</h2>
+          <Grid item xs={8}>
+            <TextField
               id="email"
               name="email"
               type="email"
               placeholder="JoeDoe@email.com"
               required
+              error={error.emailError}
+              variant="outlined"
+              label="Email"
+              helperText={error.emailError ? error.emailMessage : ""}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-          </label>
-          <label className="block text-xl py-6">
-            Password:
-            <input
+          </Grid>
+          <Grid item xs={8} mt={2}>
+            <TextField
               id="password"
               name="password"
               type="password"
               placeholder=" ◽◽◽◽◽◽◽◽"
               required
+              error={error.passwordError}
+              variant="outlined"
+              label="Password"
+              helperText={error.passwordError ? error.passwordMessage : ""}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-          </label>
-        <button
+          </Grid>
+          <Button
             className="w-full item-center bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded"
             type="submit"
-        >
-        Login
-        </button>
-      </Form>
-    </section>
-  )
+          >
+            Login
+          </Button>
+        </form>
+      </section>
+    </ThemeProvider>
+  );
 }
+// function Login() {
+//   return (
+//     <section>
+//       <Form 
+//         method="post"
+//         className="px-20 py-8 font-serif text-base/10 border rounded-5xl shadow-md content-between"
+//       >
+//         <h2 className="text-3xl font-semibold text-center mb-4">Login</h2>
+//         <label className="block text-xl py-6">
+//             Email:
+//             <input
+//               id="email"
+//               name="email"
+//               type="email"
+//               placeholder="JoeDoe@email.com"
+//               required
+//             />
+//           </label>
+//           <label className="block text-xl py-6">
+//             Password:
+//             <input
+//               id="password"
+//               name="password"
+//               type="password"
+//               placeholder=" ◽◽◽◽◽◽◽◽"
+//               required
+//             />
+//           </label>
+//         <button
+//             className="w-full item-center bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded"
+//             type="submit"
+//         >
+//         Login
+//         </button>
+//       </Form>
+//     </section>
+//   )
+// }
 
 export default Login
