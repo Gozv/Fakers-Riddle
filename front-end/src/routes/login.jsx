847d26dd96eb75da28ff1bbcccf365379bbd6813
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-22 22:02:50 -0300

    Add fetch to login

--- front-end/src/routes/login.jsx
+++ front-end/src/routes/login.jsx
@@ -3,6 +3,7 @@ import { Form, redirect } from "react-router-dom"
 export async function action ({ request }) {
   const formData = await request.formData();
   const loginData = Object.fromEntries(formData);
+
   const response = await fetch("http://localhost:3000/api/auth/login", {
     method: "POST",
     headers: {
