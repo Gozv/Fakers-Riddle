8e0dd1d926e2a57670b63795e739c30ec58aceec
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-25 09:19:15 -0300

    Add modified body validation

--- back-end/validations/userSchema.js
+++ back-end/validations/userSchema.js
@@ -20,7 +20,7 @@ export const userBodySchema = Joi.object({
     .required()
     .messages({
       'string.pattern.base':
-        'Username must contain at least one lowercase letter, one digit, and one underscore (_)',
+      'Username must contain lowercase letters, a digit and an underscore (_)',
       'string.min': 'Password must have at least {#limit} characters',
       'string.max': 'Password must not be longer than {#limit} characters'
     }),
@@ -32,11 +32,11 @@ export const userBodySchema = Joi.object({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/
     )
     .min(8)
-    .max(20)
+    .max(15)
     .required()
     .messages({
       'string.pattern.base':
-        'Username must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (@$!%*?&)',
+        'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (@$!%*?&)',
       'string.min': 'Password must have at least {#limit} characters',
       'string.max': 'Password must not be longer than {#limit} characters'
     }),
@@ -60,7 +60,7 @@ export const deleteShema = Joi.object({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/
     )
     .min(8)
-    .max(20)
+    .max(15)
     .required()
     .messages({
       'string.pattern.base':
