f947499fd1ee493628c7ba17d6f93744fc1d54a5
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-25 21:13:32 -0300

    Add changes

--- back-end/socket.js
+++ back-end/socket.js
@@ -1,32 +1,51 @@
 const socketServer = (io) => {
-  const availableRooms = new Set()
+  const rooms = new Map()
 
   io.on('connection', (socket) => {
+    const { userName } = socket.handshake.query
+
+    io.emit('availableRooms', Array.from(rooms.keys()))
     socket.on('joinRoom', (roomName) => {
       socket.join(roomName)
+
       console.log(`Usuario ${socket.id} se unió a la sala: ${roomName}`)
 
-      availableRooms.add(roomName)
+      if (!rooms.has(roomName)) {
+        rooms.set(roomName, new Set())
+      }
+      rooms.get(roomName).add(socket.id)
 
-      io.emit('availableRooms', Array.from(availableRooms))
+      io.emit('availableRooms', Array.from(rooms.keys()))
+
+      const usersCount = (roomName) => {
+        const usersInRoom = rooms.has(roomName) ? rooms.get(roomName).size : 0
+        return usersInRoom
+      }
+
+      socket.emit('usersCount', usersCount(roomName))
+
+      usersCount(roomName)
 
       socket.on('message', (body) => {
-        console.log(`Mensaje recibido: ${body} de ${socket.id}`)
+        console.log(`Mensaje recibido: ${body} de ${socket.id} de ${userName}`)
         socket.broadcast.to(roomName).emit('message', {
           body,
-          from: socket.id.slice(6)
+          from: userName,
+          id: socket.id.slice(6),
+          role: null
         })
       })
+
       socket.on('disconnect', () => {
-        console.log(`Desconectado de la sala ${roomName}`)
+        console.log(`Usuario ${socket.id} se desconectó de la sala ${roomName}`)
+        if (rooms.has(roomName)) {
+          rooms.get(roomName).delete(socket.id)
+          if (usersCount() === 0) {
+            rooms.delete(roomName)
+            io.emit('availableRooms', Array.from(rooms.keys()))
+          }
+        }
       })
-
-      const roomEmpty = !io.sockets.adapter.rooms.get(roomName)
-      if (roomEmpty) {
-        availableRooms.delete(roomName)
-
-        io.emit('availableRooms', Array.from(availableRooms))
-      }
     })
   })
 }
