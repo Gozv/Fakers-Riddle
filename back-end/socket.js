1f2504ea3a60a72d60461e75e1b70a9d42391944
Antonela Ledesma <ledesmaantonela20@gmail.com>
2023-09-26 16:03:32 -0300

    Add changes

--- back-end/socket.js
+++ back-end/socket.js
@@ -1,30 +1,25 @@
 const socketServer = (io) => {
   const rooms = new Map()
+  const users = new Map()
 
-  io.on('connection', (socket) => {
-    const { userName } = socket.handshake.query
+  io.on('connection', async (socket) => {
+    const { userName, role } = socket.handshake.query
+    socket.userName = userName
+    socket.role = role
 
     io.emit('availableRooms', Array.from(rooms.keys()))
-    socket.on('joinRoom', (roomName) => {
+    socket.on('joinRoom', async (roomName) => {
       socket.join(roomName)
 
-      console.log(`Usuario ${socket.id} se unió a la sala: ${roomName}`)
+      console.log(`Usuario ${socket.userName} se unió a la sala: ${roomName}`)
 
-      if (!rooms.has(roomName)) {
-        rooms.set(roomName, new Set())
-      }
-      rooms.get(roomName).add(socket.id)
-
+      const sockets = await io.in(roomName).fetchSockets()
+      const userNamesInRoom = sockets.map(socket => ({
+        userName: socket.userName,
+        role: socket.role
+      }))
+      io.emit('arrayUsers', Array.from(userNamesInRoom))
       io.emit('availableRooms', Array.from(rooms.keys()))
-
-      const usersCount = (roomName) => {
-        const usersInRoom = rooms.has(roomName) ? rooms.get(roomName).size : 0
-        return usersInRoom
-      }
-
-      socket.emit('usersCount', usersCount(roomName))
-
-      usersCount(roomName)
 
       socket.on('message', (body) => {
         console.log(`Mensaje recibido: ${body} de ${socket.id} de ${userName}`)
@@ -32,17 +27,25 @@ const socketServer = (io) => {
           body,
           from: userName,
           id: socket.id.slice(6),
-          role: null
+          role
         })
       })
 
       socket.on('disconnect', () => {
         console.log(`Usuario ${socket.id} se desconectó de la sala ${roomName}`)
         if (rooms.has(roomName)) {
-          rooms.get(roomName).delete(socket.id)
-          if (usersCount() === 0) {
-            rooms.delete(roomName)
-            io.emit('availableRooms', Array.from(rooms.keys()))
+          rooms.get(roomName).delete(userName)
+          io.emit('arrayUsers', Array.from(userNamesInRoom))
+          if (users.has(roomName)) {
+            users.set(
+              roomName,
+              users.get(roomName).filter((user) => user !== userName)
+            )
+            if (users.get(roomName).length === 0) {
+              users.delete(roomName)
+              rooms.delete(roomName)
+              io.emit('availableRooms', Array.from(rooms.keys()))
+            }
           }
         }
       })
